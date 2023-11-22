fbf2c28fab7abb81238ec89b7f4264a7957a640b
unorji kingsley <unorjikingsley@gmail.com>
2023-11-22 07:18:32 +0100

    Setup and test project server

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
-// console.log('server is working')
-import defaultValue from "./test-default.js";
-import { value } from "./test-named.js";
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-console.log(value);
-console.log(defaultValue);
+import express from 'express'
+const app = express()
+
+import morgan from 'morgan'
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+})
+
+app.post('/', (req, res) => {
+  console.log(req)
+  res.json({ message: 'data received', data: req.body })
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}...`)
+});
