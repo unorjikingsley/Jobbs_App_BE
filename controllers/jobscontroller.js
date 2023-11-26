02b900588302d256f94861b2abad0e8c0cab0f32
unorji kingsley <unorjikingsley@gmail.com>
2023-11-26 22:55:53 +0100

    Schema models and errors

--- controllers/jobscontroller.js
+++ controllers/jobscontroller.js
@@ -1,3 +1,5 @@
+import Job from '../models/JobModel.js';
+
 import { nanoid } from 'nanoid'
 
 let jobs = [
@@ -10,14 +12,21 @@ export const getAllJobs = async (req, res) => {
 };
 
 export const createJob = async (req, res) => {
-  const { company, position } = req.body
+  // const { company, position } = req.body;
+  // const job = await Job.create({ company, position });
 
-  if (!company || !position) {
-    return res.status(400).json({ msg: 'Provide a position or company' })
-  }
-  const id = nanoid(10)
-  const job = { id, company, position }
-  jobs.push(job)
+  // try {
+  //     const job = await Job.create(req.body)
+  //     res.status(200).json({ job })
+  // } catch (error) {
+  //   console.log(error);
+  //   res.status(500).json({ msg: 'server error' });
+  // }
+
+  // we can find async error this manually by using try and catch block which can be
+  // stressful. instead we can use the express-async error handler
+
+  const job = await Job.create(req.body)
   res.status(200).json({ job })
 }
 
---
+++ models/JobModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const JobSchema = new mongoose.Schema(
+  {
+    company: String,
+    position: String,
+    jobStatus: {
+      type: String,
+      enum: ['interview', 'declined', 'pending'],
+      default: 'pending',
+    },
+    jobTypes: {
+      type: String,
+      enum: ['full-time', 'part-time', 'internship'],
+      default: 'full-time',
+    },
+    jobLocation: {
+      type: String,
+      default: 'NYC',
+    },
+  },
+  { timestamps: true }
+)
+
+export default mongoose.model('job', JobSchema);
+
