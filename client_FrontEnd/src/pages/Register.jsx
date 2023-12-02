98029674f671002b949a837320284a682595bfef
unorji kingsley <unorjikingsley@gmail.com>
2023-12-02 17:24:06 +0100

    Setup react toastify

--- client_FrontEnd/src/pages/Register.jsx
+++ client_FrontEnd/src/pages/Register.jsx
@@ -2,6 +2,7 @@ import { Form, redirect, useNavigation, Link } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import { FormRow, Logo } from '../components';
 import customFetch from '../utils/customFetch';
+import {toast} from 'react-toastify';
 
 export const action = async ({ request }) => {
   const formData = await request.formData();
@@ -11,9 +12,10 @@ export const action = async ({ request }) => {
 
   try {
     await customFetch.post('/auth/register', data)
+    toast.success('Registration Successful')
     return redirect('/login');
   } catch (error) {
-    console.log(error);
+    toast.error(error?.response?.data?.msg)
     return error;
   }
 }
