8ac44d5bf9dc9d691682aae0106e5ee2b6e555da
unorji kingsley <unorjikingsley@gmail.com>
2023-12-06 12:20:18 +0100

    Edit Job Loader

--- client_FrontEnd/src/pages/EditJob.jsx
+++ client_FrontEnd/src/pages/EditJob.jsx
@@ -1,19 +1,28 @@
 import { FormRow, FormRowSelect } from '../components';
 import Wrapper from '../assets/wrappers/DashboardFormPage';
 import { useLoaderData } from 'react-router-dom';
-import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/contants.js';
 import { Form, useNavigation, redirect } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import customFetch from '../utils/customFetch';
 
 export const loader = async ({ params }) => {
-  return null
+  try {
+    const { data } = await customFetch.get(`/jobs/${params.id}`)
+    return data
+  } catch (error) {
+    toast.error(error?.response?.data?.msg)
+    return redirect('/dashboard/all-jobs');
+  }
+  // return null
 }
 export const action = async () => {
   return null
 }
 
 const EditJob = () => {
+  const { job } = useLoaderData();
+  console.log(job);
   return <h1>EditJob</h1>
 }
 export default EditJob
