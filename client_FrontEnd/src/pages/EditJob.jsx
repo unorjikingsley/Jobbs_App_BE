6821ba4a9fa529e88fee0c2360f622dda4144312
unorji kingsley <unorjikingsley@gmail.com>
2023-12-06 12:12:16 +0100

    Route params

--- client_FrontEnd/src/pages/EditJob.jsx
+++ client_FrontEnd/src/pages/EditJob.jsx
@@ -1,6 +1,19 @@
+import { FormRow, FormRowSelect } from '../components';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { useLoaderData } from 'react-router-dom';
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
+import { Form, useNavigation, redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
+
+export const loader = async ({ params }) => {
+  return null
+}
+export const action = async () => {
+  return null
+}
+
 const EditJob = () => {
-  return (
-    <h1>EditJob</h1>
-  )
+  return <h1>EditJob</h1>
 }
 export default EditJob
