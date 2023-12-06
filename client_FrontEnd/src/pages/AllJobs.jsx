4cba661f946f0d4de7c5fdb6aacc477571b43a13
unorji kingsley <unorjikingsley@gmail.com>
2023-12-06 08:02:43 +0100

    Add job structure

--- client_FrontEnd/src/pages/AllJobs.jsx
+++ client_FrontEnd/src/pages/AllJobs.jsx
@@ -1,6 +1,29 @@
+import { toast } from 'react-toastify'
+import { JobsContainer, SearchContainer } from '../components'
+import customFetch from '../utils/customFetch'
+import { useLoaderData } from 'react-router-dom'
+import { useContext, createContext } from 'react';
+
+export const loader = async () => {
+  try {
+    const { data } = await customFetch.get('/jobs');
+    return { data };
+  } catch (error) {
+    toast.error(error?.response?.data?.msg)
+    return error;
+  }
+};
+
 const AllJobs = () => {
+  const { data } = useLoaderData();
+  console.log(data);
+
   return (
-    <h1>AllJobs</h1>
+    <>
+      <SearchContainer />
+      <JobsContainer />
+    </>
   )
 }
-export default AllJobs
+
+export default AllJobs;
