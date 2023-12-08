c29351c65c9d7b24dbe6d3e7170292daeccd78d0
unorji kingsley <unorjikingsley@gmail.com>
2023-12-08 19:29:20 +0100

    Update user functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
 import Job from "../models/JobModel.js";
+import cloudinary from 'cloudinary';
+import { promises as fs } from 'fs';
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId });
@@ -14,9 +16,20 @@ export const getApplicationStats = async (req, res) => {
   res.status(StatusCodes.OK).json({ users, jobs });
 };
 export const updateUser = async (req, res) => {
-  const obj = { ...req.body };
-  delete obj.password;
+  const newUser = { ...req.body };
+  delete newUser.password;
 
-  const updatedUser = await User.findByIdAndUpdate(req.user.userId, req.body);
+  if(req.file) {
+    const response = await cloudinary.v2.uploader.upload(req.file.path);
+    await fs.unlink(req.file.path);
+    newUser.avatar = response.secure_url
+    newUser.avatarPublicId = response.public_id;
+  }
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.userId, newUser);
+
+  if (req.file && updatedUser.avartarPublicId) {
+    await cloudinary.v2.uploader.destroy(updatedUser.avartarPublicId);
+  }
   res.status(StatusCodes.OK).json({ msg: 'update user' });
 };
