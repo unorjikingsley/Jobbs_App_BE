db74fc0ef4b02aa70b3c8baea400d1a1a0b03937
unorji kingsley <unorjikingsley@gmail.com>
2023-12-08 17:14:01 +0100

    Set up Multer for images upload

--- client_FrontEnd/src/pages/Profile.jsx
+++ client_FrontEnd/src/pages/Profile.jsx
@@ -15,7 +15,7 @@ export const action = async({ request }) => {
   }
 
   try {
-    await customFetch.patch('/user/update-user', formData)
+    await customFetch.patch('/users/update-user', formData)
     toast.success('Profile updated successfully');
   } catch (error) {
     toast.error(error?.response?.data?.msg);
--- controllers/userController.js
+++ controllers/userController.js
@@ -16,6 +16,7 @@ export const getApplicationStats = async (req, res) => {
 export const updateUser = async (req, res) => {
   const obj = { ...req.body };
   delete obj.password;
+
   const updatedUser = await User.findByIdAndUpdate(req.user.userId, req.body);
   res.status(StatusCodes.OK).json({ msg: 'update user' });
 };
