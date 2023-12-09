a19c560d1fce034f0795b0fc80da6e0025d40a21
unorji kingsley <unorjikingsley@gmail.com>
2023-12-09 09:42:28 +0100

    Restricted Access To test/demo users

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,7 +5,10 @@ import {
   updateUser,
 } from '../controllers/userController.js';
 import { validateUpdateUserInput } from '../middleware/validationMiddleware.js';
-import { authorizePermissions } from '../middleware/authMiddleware.js';
+import {
+  authorizePermissions,
+  checkForTestUser,
+} from '../middleware/authMiddleware.js'
 
 import upload from '../middleware/multerMiddleware.js';
 
@@ -13,6 +16,12 @@ const router = Router();
 
 router.get('/current-user', getCurrentUser);
 router.get('/admin/app-stats', authorizePermissions('admin'), getApplicationStats);
-router.patch('/update-user', upload.single('avatar'), validateUpdateUserInput, updateUser);
+router.patch(
+  '/update-user',
+  checkForTestUser,
+  upload.single('avatar'),
+  validateUpdateUserInput,
+  updateUser
+);
 
 export default router;
