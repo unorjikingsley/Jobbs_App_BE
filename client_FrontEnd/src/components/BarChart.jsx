556a23a6ee89e55f2527e2dbf006b808581d627f
unorji kingsley <unorjikingsley@gmail.com>
2023-12-09 16:59:32 +0100

    Created bar chart and read chart

--- client_FrontEnd/src/components/BarChart.jsx
+++ client_FrontEnd/src/components/BarChart.jsx
@@ -1,6 +1,26 @@
-const BarChart = () => {
+/* eslint-disable react/prop-types */
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
+
+const BarChartComponent = ({ data }) => {
   return (
-    <div>BarChart</div>
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3 " />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Bar dataKey="count" fill="#2cb1bc" barSize={75} />
+      </BarChart>
+    </ResponsiveContainer>
   )
 }
-export default BarChart
+
+export default BarChartComponent;
