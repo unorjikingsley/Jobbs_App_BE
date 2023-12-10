50f6a1b7345dbd903a7c389f789a78fd78621048
unorji kingsley <unorjikingsley@gmail.com>
2023-12-10 12:39:46 +0100

    Search params

--- controllers/jobscontroller.js
+++ controllers/jobscontroller.js
@@ -4,11 +4,22 @@ import mongoose from 'mongoose';
 import day from 'dayjs';
 
 export const getAllJobs = async (req, res) => {
-  console.log(req.query);
-  const jobs = await Job.find({
+  // console.log(req.query);
+
+  const {search} = req.query;
+
+  const queryObject = {
     createdBy: req.user.userId,
-    position: req.query.search
-  })
+  }
+
+  if(search) {
+    queryObject.$or = [
+      { position: { $regex: search, $options: 'i' } },
+      { company: { $regex: search, $options: 'i' } },
+    ]
+  }
+
+  const jobs = await Job.find(queryObject)
   res.status(StatusCodes.OK).json({ jobs })
 };
 
