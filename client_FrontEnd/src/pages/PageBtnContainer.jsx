19f52e1fb0a385a296a6c28f73f6c929a0828d2e
unorji kingsley <unorjikingsley@gmail.com>
2023-12-13 08:57:28 +0100

    Pagination setup

---
+++ client_FrontEnd/src/pages/PageBtnContainer.jsx
@@ -0,0 +1,11 @@
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi';
+import Wrapper from '../assets/wrappers/PageBtnContainer';
+import { useLocation, Link, useNavigate } from 'react-router-dom';
+import { useAllJobsContext } from '../pages/AllJobs';
+
+const PageBtnContainer = () => {
+  const { data: {numOfPages, currentPage }} = useAllJobsContext()
+  console.log(numOfPages, currentPage);
+  return <div>PageBtnContainer</div>
+}
+export default PageBtnContainer
