d66caaa77aed955dc80759c61973addbca7dd73d
unorji kingsley <unorjikingsley@gmail.com>
2023-12-14 08:38:45 +0100

    Styled bpagination buttons

--- client_FrontEnd/src/assets/wrappers/PageBtnContainer.js
+++ client_FrontEnd/src/assets/wrappers/PageBtnContainer.js
@@ -8,11 +8,13 @@ const Wrapper = styled.section`
   justify-content: end;
   flex-wrap: wrap;
   gap: 1rem;
+
   .btn-container {
     background: var(--background-secondary-color);
     border-radius: var(--border-radius);
     display: flex;
   }
+
   .page-btn {
     background: transparent;
     border-color: transparent;
@@ -24,36 +26,38 @@ const Wrapper = styled.section`
     border-radius: var(--border-radius);
     cursor:pointer:
   }
+
   .active{
     background:var(--primary-500);
         color: var(--white);
-
   }
+
   .prev-btn,.next-btn{
     background: var(--background-secondary-color);
     border-color: transparent;
-        border-radius: var(--border-radius);
-
+    border-radius: var(--border-radius);
     width: 100px;
     height: 40px;
-        color: var(--primary-500);
-text-transform:capitalize;
-letter-spacing:var(--letter-spacing);
-display:flex;
-align-items:center;
-justify-content:center;
-gap:0.5rem;
-cursor:pointer;
+    color: var(--primary-500);
+    text-transform:capitalize;
+    letter-spacing:var(--letter-spacing);
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    gap:0.5rem;
+    cursor:pointer;
   }
+
   .prev-btn:hover,.next-btn:hover{
     background:var(--primary-500);
-        color: var(--white);
-        transition:var(--transition);
+    color: var(--white);
+    transition:var(--transition);
   }
-.dots{
-  display:grid;
-  place-items:center;
-  cursor:text;
-}
+
+  .dots{
+    display:grid;
+    place-items:center;
+    cursor:text;
+  }
 `;
 export default Wrapper;
--- client_FrontEnd/src/pages/PageBtnContainer.jsx
+++ client_FrontEnd/src/pages/PageBtnContainer.jsx
@@ -1,6 +1,6 @@
 import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi';
 import Wrapper from '../assets/wrappers/PageBtnContainer';
-import { useLocation, Link, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useAllJobsContext } from '../pages/AllJobs';
 
 const PageBtnContainer = () => {
