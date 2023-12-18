384fed1255f81d8363ea10639586253b24409fc9
unorji kingsley <unorjikingsley@gmail.com>
2023-12-18 15:39:20 +0100

    FIX UPDATE USER

--- server.js
+++ server.js
@@ -39,7 +39,8 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.static(path.resolve(__dirname, './public')))
+// app.use(express.static(path.resolve(__dirname, './public')))
+app.use(express.static(path.resolve(__dirname, './client_FrontEnd/dist')))
 
 app.use(cookieParser());
 app.use(express.json());
@@ -55,6 +56,10 @@ app.get('/api/v1/test', (req, res) => {
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
+
+app.use('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client_FrontEnd/dist', 'index.html'));
+})
 
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'Resources not found' })
